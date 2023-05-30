b5ad230cd79f5f055f8fd41d4bd28958950e9691
4mnesia <fla.henriquez@duocuc.cl>
2023-05-30 18:18:18 -0400

    Create Ejercicios3,2.py

---
+++ Ejercicios3,2.py
@@ -0,0 +1,32 @@
+"""
+#multiplos con la lista
+lista = []
+numero = int(input("ingrese un numero: "))
+
+for i in range(1,11):
+    lista.append(numero*i)
+print(lista)
+"""
+"""
+#random.randint(): genera numeros aleatorios
+import random
+lista = []
+numero = int(input("ingrese un numero: "))
+
+for i in range(1,11):
+    print(random.randint(1,500))
+    lista.append(numero*i)
+print(lista)
+"""
+#primer ejercicio
+numero = int(input("ingrese un numero: "))
+list = []
+for in range(1,11):
+    list.append = [numero * i]
+#segundo ejercicio
+numero = int(input("ingrese un numero"))
+ 
+for i in range(1,21):
+    list = [numero*i]
+    if numero == 0:
+        break
