8ee8bb60f49fe4d8ba1b122368647b3e11caa8d3
4mnesia <fla.henriquez@duocuc.cl>
2023-06-19 01:14:00 -0400

    Update Ejercicios pre prueba.py

--- Ejercicios pre prueba.py
+++ Ejercicios pre prueba.py
@@ -1,52 +1,54 @@
 import numpy as np
+
 print("Bienvenidos a VuelosDuoc")
 print("1.- Entradas normales")
 print("2.- Entradas VIP")
 print("3.- Salir/Exit")
-opcion= int(input("Seleccione un tipo de entrada: "))
-Anormal = np.arange(1,31)
-Apremiun = np.arange(31,43)
-while opcion != 3:
-    print("Estos asientos pertenecen a las pasajeros con entrada normal ")
-    print(Anormal)
-    print("Y estos son para entrada premiun ")
-    print(Apremiun)
+valornormal = 78900
+valorpremiun = 240000
+Anormal = np.arange(1, 31)
+Apremiun = np.arange(31, 43)
 
+opcion = int(input("Seleccione un tipo de entrada: "))
+
+while opcion != 3:
     if opcion == 1:
         print("Escogiste normal")
+        print("Estos asientos pertenecen a los pasajeros con entrada normal:")
         print(Anormal)
         Anormal = Anormal.reshape((6, 5))
         Anormal.sort(axis=1)
         print(Anormal)
-        dato = int(input("Indique el asiento que quiere"))
-        if dato < 1:
-            print("opcion invalida")
-        if dato >= 30:
-            print("te pasaste wey")
-    if opcion == 2:
+        dato = int(input("Indique el asiento que desea: "))
+        
+        if dato < 1 or dato >= 31:
+            print("Opción inválida")
+        else:
+            if dato in Anormal:
+                Anormal = np.delete(Anormal, np.where(Anormal == dato))
+                print("Asiento seleccionado con éxito.")
+            else:
+                print("El asiento no está disponible.")
+        
+    elif opcion == 2:
         print("Escogiste Premiun")
+        print("Estos asientos pertenecen a los pasajeros con entrada premiun:")
         print(Apremiun)
         Apremiun = Apremiun.reshape((2, 6))
         Apremiun.sort(axis=1)
         print(Apremiun)
-        dato = int(input("Indique el asiento que quiere"))
-        if dato < 30:
-            print("opcion invalida")
-        if dato >= 42:
-            print("te pasaste wey")
+        dato = int(input("Indique el asiento que desea: "))
+        
+        if dato < 31 or dato >= 43:
+            print("Opción inválida")
+        else:
+            if dato in Apremiun:
+                Apremiun = np.delete(Apremiun, np.where(Apremiun == dato))
+                print("Asiento seleccionado con éxito.")
+            else:
+                print("El asiento no está disponible.")
+        
     else:
-        print("see you later")
-        
-
-
-        
-        
-
-
-        
-
-
-
-
-
-
+        print("See you later")
+    
+    opcion = int(input("Seleccione un tipo de entrada: "))
